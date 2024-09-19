302eeaca1afd860ab58c0d93821f367c4085e9eb
Luan <luanbatalha809@gmail>
2024-09-19 10:10:35 -0400

    Dexei o site responsivo

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -4,9 +4,7 @@ export default function Modal({isOpen}) {
 
     if(isOpen){
         return (
-            <div className='modal'>
-                <p className='modal-paragraph'>"Enter the word you want to search for."</p>
-            </div>
+                <p className='modal-paragraph'>"Enter the word you want to search for. The dictionary project aims to create a dynamic and interactive platform for consulting word definitions and meanings. The goal is to offer a practical and easy-to-use tool that allows users to quickly search for verbs, nouns, synonyms, adjectives, etc."</p>
           )
     }else{
         return null
